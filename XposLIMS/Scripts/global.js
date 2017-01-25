e7c13ff1e5a0e2cd332b62c4b8268a03b33fa822
asultan80 <alex60@gmail.com>
2017-01-25 12:28:03 -0400

    General Navigation between Views
    We can simply change the text to not show percentages, but just a
    “Please be patient” message that disappears at the end of the given
    process.

--- XposLIMS/Scripts/global.js
+++ XposLIMS/Scripts/global.js
@@ -2,4 +2,21 @@
     $("#manageLink").on("click", function() {
         $("iframe", "#ManageLogin").attr("src", "/Manage");
     });
+
+    $("a.progressbar-loading").on("click", function (e) {
+        e.preventDefault();
+        var href = $(this).attr("href");
+
+        showLoadingProgressbar();
+
+        setTimeout(function() {
+            window.location.href = href;
+        }, 500);
+
+        return false;
+    });
+
+    function showLoadingProgressbar() {
+        $("#curtain").show();
+    }
 });
